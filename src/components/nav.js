18933a5bb08ffe53489c06011e7715486db7237c
Adegbuyi Jephthah <114796977+PrinceJetro@users.noreply.github.com>
2024-05-13 22:54:15 +0100

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,7 @@ export default function Navbar(){
     return(
         <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">PrinceJetro Web Dev Training</a>
+          <a className="navbar-brand" href="#">PrinceJetro Web Training</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
